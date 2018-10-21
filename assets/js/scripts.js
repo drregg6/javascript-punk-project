04310cd5c3043ce5642767fb2d2387f33e60c940
Dave <drregg6@gmail.com>
2018-10-21 18:17:00 -0400

    Simple API call with vanilla JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+const endpoint = 'https://api.punkapi.com/v2/beers/random';
+
+function test() {
+    fetch(endpoint)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        results = data.results[0];
+        console.log(results);
+    });
+}
+
+test();
