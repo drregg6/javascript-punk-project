85ab91adf73b51988a6f437ff420a1f8671b58a8
Dave <drregg6@gmail.com>
2018-10-22 08:40:29 -0400

    Form added, broken req query in app.js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs');
 
 app.use(express.static(path.join(__dirname)));
 app.use('/styles', express.static(__dirname));
 app.use('/images', express.static(__dirname));
 app.use('/scripts', express.static(__dirname));
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+    if (typeof req.query['search'] !== 'undefined') {
+        let test = req.query['search'];
+    }
+    
+    res.render('index', { test : test });
 });
 
 app.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/about.html'));
+    res.render('about');
 });
 
 app.listen(8080, function() {
